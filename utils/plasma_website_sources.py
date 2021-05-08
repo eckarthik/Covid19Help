98339bf0190e8ec938d22a1c2ac51f055ab37078
sumanth1830 <sumanth1830@gmail.com>
2021-05-08 14:37:08 +0530

    Added Plasma API

---
+++ utils/plasma_website_sources.py
@@ -0,0 +1,16 @@
+PLASMA_DETAILS = {
+    "Dhoondh": "https://dhoondh.com/",
+    "Pintnetwork": "https://pintnetwork.com/patient",
+    "NeedPlasma": "https://needplasma.in/",
+    "TeamSOSIndia": "https://www.teamsosindia.in/covidplasmarequest.html",
+    "PlasmaDonor": "https://plasmadonor.in/",
+    "Covid19 Plasma Facebook Group": "https://m.facebook.com/groups/556579325240584",
+    "Plasmaline": "https://plasmaline.in/",
+    "GetPlasma": "https://getplasma.in/FindDonor.php",
+    "Inara": "http://inaraa.org/donor-filter",
+    "Delhi Fights Corona": "https://delhifightscorona.in/donateplasma/",
+    "Find Plasma Donor Gujarat": "https://www.findplasmadonor.com/",
+    "Telangana Plasma Donors": "https://telanganaplasmadonors.com/",
+    "GiveRed": "https://givered.in/",
+    "Pune Plasma": "https://puneplasma.in/"
+}
